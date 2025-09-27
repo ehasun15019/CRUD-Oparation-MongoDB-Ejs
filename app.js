6b7eeedfc4c90476fc43098d2a6c9815db5bfcca
ehasun15019 <ehasunulislam0202@gmail.com>
2025-09-28 01:20:40 +0600

    setup express in app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// server port 
+const PORT = 3000;
+
+app.get("/", (req, res)=> {
+    res.send("Hello");
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`);  
+})
